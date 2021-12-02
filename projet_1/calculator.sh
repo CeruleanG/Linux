a98ec79878f7921ada5e139a9a44d3a91dbd6a71
CeruleanG <84096851+CeruleanG@users.noreply.github.com>
2021-12-01 23:47:38 -1000

    Add files via upload

---
+++ projet_1/calculator.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+echo "This is an arithmetical calculator."
+contin=1
+while [ $contin == 1 ] 
+do
+	a=""
+	while [ -z "$a" ]
+	do
+		echo "Please enter the first value:"
+		echo "a="
+		read a
+	done
+	b=""
+	while [ -z "$b" ]
+	do
+		echo "Please enter the second value:"
+		echo "b="
+		read b
+	done
+	echo "Please enter the operator +,-,*,/:"
+	read operation
+
+	case $operation in
+		"+")
+			let ans=a+b
+			echo $ans
+			;;
+		"-")
+			let ans=a-b
+			echo $ans
+			;;
+		"*")
+			let ans=a*b
+			echo $ans
+			;;
+		"/")
+			if [ $b == 0 ]
+			then
+				echo "error:divided by 0"
+			else
+				printf %.3f "$((10**3 * a/b))e-3"
+				echo " "
+			fi
+			;;
+			
+		*)
+			echo "error: operation not recongized"
+			;;
+	esac
+			
+	echo "enter 1 if you want to continue,"
+	echo "enter 0 if you want to quit."
+	read contin
+done
